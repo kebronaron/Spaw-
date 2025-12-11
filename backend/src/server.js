06e4b011c5d90bf484cd64d809ca57d95bbe5890
kebronaron <kebron644@gmail.com>
2025-12-11 00:06:52 -0600

    Update CORS for production deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"], // frontend (dev ports)
+    origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,               // allow cookies
   })
 );
